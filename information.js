c93052356d6266ae70e855d9f64481febfb05aa7
anais-ctnt <anaiscotinet@gmail.com>
2020-10-01 16:27:19 +0200

    change object

--- information.js
+++ information.js
@@ -1,11 +1,13 @@
-const name = 'Anaïs'
-let campus = 'Nantes'
+const wilder = {
+  name: 'Anaïs',
+  campus: 'Nantes',
+}
 
   const myPresentation = () => {
     const cowsay = require("cowsay");
     console.log(cowsay.say({
-  	text : `Hello, I'm ${name} from ${campus} Campus !`,
+  	text : `Hello, I'm ${wilder.name} from ${wilder.campus} Campus !`,
   }));
   };
-
+  
 module.exports = myPresentation;
